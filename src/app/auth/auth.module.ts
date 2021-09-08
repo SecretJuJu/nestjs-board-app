57cd6007ff98baf698aed0c1225f18b89db03ca8
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-09-08 18:50:57 +0900

    add: auth module 에서 UserRepository를 import

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserRepository } from './user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   providers: [AuthService],
   controllers: [AuthController],
 })
