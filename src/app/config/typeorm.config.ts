66d981b880be8b5ea13cac9865924f9c87c8868b
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-09-08 19:20:21 +0900

    chore: typeorm config 파일 작성

--- src/app/config/typeorm.config.ts
+++ src/app/config/typeorm.config.ts
@@ -1,7 +1,15 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import env from './env'
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import env from './env';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    host: 
-}
+  type: 'postgres',
+  host: env.DB_HOST,
+  port: Number(env.DB_PORT),
+  username: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
+  entities: [__dirname + '../**/*.entitiy.{js,ts}'],
+  // TODO: production시 false로 바꿔야함
+  // production 모드일때 false로 바뀌는 로직 필요
+  synchronize: true,
+};
