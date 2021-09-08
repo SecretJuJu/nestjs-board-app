e10b3e4e3b34beeeeca80188e17f309cbc77b789
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-09-08 19:36:06 +0900

    add: service createUser 메서드 작성

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -7,4 +9,8 @@ export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
   ) {}
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const newUser: User = await this.userRepository.createUser(createUserDto);
+    return newUser;
+  }
 }
