548e117dfe1a0bff0b478293ffc4880e3eec8b23
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-09-15 11:02:26 +0900

    add: create user auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async register(createUserDto: CreateUserDto) {
+    const createdUser = await this.userService.createUser(createUserDto);
+    const { id: userId } = createdUser;
+    return userId;
+  }
+}
